e91b7ade574181945971e98fd765f13754dac6dd
Paolo Sasso <sasso@mac.com>
2024-12-01 11:22:10 +0000

    Ready

--- src/y2024/day01/AoC01.kt
+++ src/y2024/day01/AoC01.kt
@@ -1,21 +1,30 @@
 package y2024.day01
 
 import Day
+import kotlin.math.abs
 
 
 fun main() {
 	AOC1
 }
 
-private object AOC1: Day<Int, Int>(1, 1, 1, 1) {
+private object AOC1: Day<Int, Int>(11, 31, 1938424, 22014209) {
 
 	init {
-		part1Text { txt ->
-			1
+		part1Lines { lines ->
+			val pairs = lines.map { it.split("   ").map(String::toInt) }
+			val l1 = pairs.map { it[0] }.sorted()
+			val l2 = pairs.map { it[1] }.sorted()
+			val zip = l1.zip(l2)
+
+			zip.sumOf { abs(it.first - it.second) }
 		}
 
-		part2Text { txt ->
-			1
+		part2Lines { lines ->
+			val pairs = lines.map { it.split("   ").map(String::toInt) }
+			val l1 = pairs.map { it[0] }.sorted()
+			val l2 = pairs.map { it[1] }.sorted()
+			l1.sumOf { first -> first * l2.count { it == first } }
 		}
 	}
 }
