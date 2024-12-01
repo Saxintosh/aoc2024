498a5f22df719cbe5100dee198bd0c53cad59e10
Paolo Sasso <sasso@mac.com>
2024-12-01 23:45:26 +0000

    Day 01 - Refactoring

--- src/y2024/day01/AoC01.kt
+++ src/y2024/day01/AoC01.kt
@@ -34,17 +34,18 @@ private object AOC1: Day<Int, Int>(11, 31, 1938424, 22014209) {
 
 		part2Lines { lines ->
 			val pairs = lines.map { it.split("   ").map(String::toInt) }
-			val l1 = pairs.map { it[0] }.sorted()
-			val l2 = pairs.map { it[1] }.sorted()
+			val l1 = pairs.map { it[0] }
+			val l2 = pairs.map { it[1] }
 			l1.sumOf { first -> first * l2.count { it == first } }
 		}
 
 		part2Lines { lines ->
-			val pairs = lines.map { it.split("   ").map(String::toInt) }
-			val l1 = pairs.map { it[0] }.sorted()
-			val l2 = pairs.map { it[1] }.sorted()
-			val l2Group = l2.groupingBy { it }.eachCount()
-			l1.sumOf { it * l2Group.getOrDefault(it, 0) }
+			val (l1, l2) = lines.map { line: String ->
+				line.split("   ")
+					.let { it[0].toInt() to it[1].toInt() }
+			}.unzip()
+			val frequencies = l2.groupingBy { it }.eachCount()
+			l1.sumOf{ it * frequencies.getOrDefault(it, 0) }
 		}
 	}
 }
