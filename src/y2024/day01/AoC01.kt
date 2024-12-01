c1ec4bd2e37c3853de282bdf0fbc5e4a9491ad1a
Paolo Sasso <sasso@mac.com>
2024-12-01 23:30:01 +0000

    Day 01 - Refactoring

--- src/y2024/day01/AoC01.kt
+++ src/y2024/day01/AoC01.kt
@@ -11,6 +11,7 @@ fun main() {
 private object AOC1: Day<Int, Int>(11, 31, 1938424, 22014209) {
 
 	init {
+
 		part1Lines { lines ->
 			val pairs = lines.map { it.split("   ").map(String::toInt) }
 			val l1 = pairs.map { it[0] }.sorted()
@@ -20,11 +21,30 @@ private object AOC1: Day<Int, Int>(11, 31, 1938424, 22014209) {
 			zip.sumOf { abs(it.first - it.second) }
 		}
 
+		part1Lines { lines ->
+			lines
+				.map {
+					val nums = it.split("   ").map(String::toInt)
+					nums[0] to nums[1]
+				}
+				.unzip()
+				.let { it.first.sorted() zip it.second.sorted() }
+				.sumOf { abs(it.first - it.second) }
+		}
+
 		part2Lines { lines ->
 			val pairs = lines.map { it.split("   ").map(String::toInt) }
 			val l1 = pairs.map { it[0] }.sorted()
 			val l2 = pairs.map { it[1] }.sorted()
 			l1.sumOf { first -> first * l2.count { it == first } }
+		}
+
+		part2Lines { lines ->
+			val pairs = lines.map { it.split("   ").map(String::toInt) }
+			val l1 = pairs.map { it[0] }.sorted()
+			val l2 = pairs.map { it[1] }.sorted()
+			val l2Group = l2.groupingBy { it }.eachCount()
+			l1.sumOf { it * l2Group.getOrDefault(it, 0) }
 		}
 	}
 }
