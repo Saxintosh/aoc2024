48ce8fe95e088b2962963d59797ff11401b002c1
Paolo Sasso <sasso@mac.com>
2024-12-03 23:33:14 +0000

    Day 03 - use of it.destructured

--- src/y2024/day03/AoC03.kt
+++ src/y2024/day03/AoC03.kt
@@ -8,7 +8,7 @@ fun main() {
 }
 
 
-private object AOC3: Day<Int, Int>(161, 48, 178794710, 76729637, true) {
+private object AOC3 : Day<Int, Int>(161, 48, 178794710, 76729637, true) {
 
 	val regex = Regex("""mul\((\d+),\s*(\d+)\)""")
 	val regex2 = Regex("""mul\((\d+),\s*(\d+)\)|don't\(\)|do\(\)""")
@@ -17,12 +17,10 @@ private object AOC3: Day<Int, Int>(161, 48, 178794710, 76729637, true) {
 		part1Text("isSafe") { txt ->
 			regex
 				.findAll(txt)
-				.map {
-					val x = it.groupValues[1].toInt()
-					val y = it.groupValues[2].toInt()
-					x * y
+				.sumOf {
+					val (x, y) = it.destructured
+					x.toInt() * y.toInt()
 				}
-				.sum()
 		}
 
 
@@ -31,8 +29,8 @@ private object AOC3: Day<Int, Int>(161, 48, 178794710, 76729637, true) {
 			regex2.findAll(txt).fold(0) { acc: Int, match: MatchResult ->
 				when (match.value) {
 					"don't()" -> acc.also { guard = false }
-					"do()"    -> acc.also { guard = true }
-					else      -> if (guard) acc + match.groupValues[1].toInt() * match.groupValues[2].toInt() else acc
+					"do()" -> acc.also { guard = true }
+					else -> if (guard) acc + match.groupValues[1].toInt() * match.groupValues[2].toInt() else acc
 				}
 			}
 		}
