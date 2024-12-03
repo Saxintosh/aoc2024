ab5030ff027ed676c1229f868bb6d59412859eff
Paolo Sasso <sasso@mac.com>
2024-12-03 23:40:04 +0000

    ...

--- src/y2024/day03/AoC03.kt
+++ src/y2024/day03/AoC03.kt
@@ -8,7 +8,7 @@ fun main() {
 }
 
 
-private object AOC3 : Day<Int, Int>(161, 48, 178794710, 76729637, true) {
+private object AOC3: Day<Int, Int>(161, 48, 178794710, 76729637, true) {
 
 	val regex = Regex("""mul\((\d+),\s*(\d+)\)""")
 	val regex2 = Regex("""mul\((\d+),\s*(\d+)\)|don't\(\)|do\(\)""")
@@ -23,14 +23,13 @@ private object AOC3 : Day<Int, Int>(161, 48, 178794710, 76729637, true) {
 				}
 		}
 
-
 		part1Text("with Wrapper") { txt ->
 			var guard = true
 			regex2.findAll(txt).fold(0) { acc: Int, match: MatchResult ->
 				when (match.value) {
 					"don't()" -> acc.also { guard = false }
-					"do()" -> acc.also { guard = true }
-					else -> if (guard) acc + match.groupValues[1].toInt() * match.groupValues[2].toInt() else acc
+					"do()"    -> acc.also { guard = true }
+					else      -> if (guard) acc + match.groupValues[1].toInt() * match.groupValues[2].toInt() else acc
 				}
 			}
 		}
